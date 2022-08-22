164a9d2dfa93f59bd81deb6ccc158f4a76874a06
renangiaretta <renan.giaretta@hotmail.com>
2022-08-22 13:28:00 -0300

    1.1

---
+++ script.js
@@ -0,0 +1,9 @@
+function encontraValor (){
+    let listaNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    let valorNum = 10
+    for(i=1; i<=listaNum.length; i++){
+
+    }
+    return numeros
+}
+console.log(listanum.find(valorNum))
